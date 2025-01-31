92436b6300b094ed05b0896d66ec9272a9a527e8
fhrk_ <78fhruby78@gmail.com>
2025-02-01 02:33:14 +0900

    熟語機能

--- src/constsant.ts
+++ src/constsant.ts
@@ -1,4 +1,5 @@
 export const apiUri = "https://script.google.com/macros/s/AKfycbyza2dErTrE1xpzXYmAzxFk3je4komShmmaoDXx45_ZVnsv1vfE2DJrnAzjHbNgklO64Q/exec"
 export type imigoApiResponse = {
-    イミ語辞書: [[string, string, string, string]]
+    イミ語辞書: [[string, string, string, string]],
+    イミ語熟語: [[string, string, string, string]]
 }
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,36 @@ import "./style.css"
 
 let isLoaded = false
 let dict: [string, string, string, string][] = []
+let collocationDict: [string, string, string, string][] = []
 
 function findWord(value: string) {
   let result = []
   for (const word of dict) {
     if (word[0].includes(value) || word[1].includes(value)) {
-      result.push(word)
+      result.push([word[0], word[2], word[1], word[3]])
+    }
+  }
+  for (const word of collocationDict) {
+    if (word[0].includes(value) || word[1].includes(value)) {
+      result.push([word[0], '熟語', word[1], word[2]])
     }
   }
   return result
+}
+
+function showResult(inputword: string) {
+  if (!isLoaded) alert('読み込み中です！')
+  rb.innerHTML = ''
+  findWord(inputword).forEach((word) => {
+    rb.insertAdjacentHTML('beforeend', `
+      <div class="word">
+        <span class="word_title">${word[0]}</span>
+        <span class="word_type">${word[1]}</span>
+        <p class="word_mean">${word[2]}</p>
+        <p class="word_description">${word[3]}</p>
+      </div>
+    `.trim())
+  })
 }
 
 const si = document.querySelector('#search') as HTMLInputElement
@@ -19,34 +40,15 @@ const sb = document.querySelector('#search-button') as HTMLButtonElement
 const rb = document.querySelector('#result') as HTMLDivElement
 
 sb.addEventListener('click', () => {
-  if (!isLoaded) alert('読み込み中です！')
-  rb.innerHTML = ''
-  findWord(si.value).forEach((word) => {
-    rb.insertAdjacentHTML('beforeend', `
-      <div class="word">
-        <span class="word_title">${word[0]}</span>
-        <span class="word_type">${word[2]}</span>
-        <p class="word_mean">${word[1]}</p>
-        <p class="word_description">${word[3]}</p>
-      </div>
-    `.trim())
-  })
+  showResult(si.value)
 })
 
 fetch(apiUri)
   .then((res) => res.json())
   .then((data: imigoApiResponse) => {
-    dict = data.イミ語辞書.slice(1).sort()
+    dict = data.イミ語辞書.slice(1)
+    collocationDict = data.イミ語熟語.slice(1)
     isLoaded = true
     document.querySelector('.warn')?.classList.add('close')
-    dict.forEach((word) => {
-      rb.insertAdjacentHTML('beforeend', `
-        <div class="word">
-          <span class="word_title">${word[0]}</span>
-          <span class="word_type">${word[2]}</span>
-          <p class="word_mean">${word[1]}</p>
-          <p class="word_description">${word[3]}</p>
-        </div>
-      `.trim())
-    })
+    showResult('')
   })
